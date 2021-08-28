3dd34e28f9aae89368ad8317aa0c9b8d406d2354
Anany-Gupta <ananygupta@gmail.com>
2021-08-28 22:26:17 +0530

    Added page routing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import {
+	BrowserRouter as Router,
+	Redirect,
+	Route,
+	Switch,
+  } from "react-router-dom";
+import AccountPage from "./Components/AccountPage";
+import Settings from './Components/Settings'
 import './App.css';
 import React, { useState } from 'react';
 import GenerateText from './Components/GenerateText';
@@ -5,12 +13,28 @@ import Navbar from './Components/Navbar';
 
 let App = () => {
 	const [paraIndex, setParaIndex] = useState(0);
-	const state = { paraLengths: [10, 20, 30, 50], paraIndex, setParaIndex };
+	const state = { paraLengths: [10, 20, 30, 50,100], paraIndex, setParaIndex };
 	return (
-		<>
+		<Router>
+
+		<>  
+
+		
 			<Navbar {...state} />
-			<GenerateText {...state} />
+			<Switch>
+				<Route path="/" exact>
+					<GenerateText {...state} />
+				</Route>
+				<Route path="/settings" exact>
+					<Settings/>
+				</Route>
+				<Route path="/account" exact>
+					<AccountPage/>
+				</Route>
+
+			</Switch>
 		</>
+		</Router>
 	);
 };
 
---
+++ src/Components/AccountPage.jsx
@@ -0,0 +1,16 @@
+                                                                                  
+let AccountPage = () => {
+  return (
+    <>
+      <div className="signup">
+		 <h1>Signup</h1>
+	  </div>
+	  <div className="Login">
+		 <h1>Login</h1>
+
+	  </div>
+    </>
+  );
+};
+
+export default AccountPage;
