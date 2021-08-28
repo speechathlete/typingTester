d12d36cedefc34b9446a04f3b441ca5845837fc3
Vatsal Gupta <vatsalgupta280@gmail.com>
2021-08-28 17:04:03 +0530

    added key

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -13,9 +13,9 @@ let Navbar = ({ paraLengths, paraIndex, setParaIndex }) => (
 			<h4>Words</h4>
 			<div className="set-count">
 				{paraLengths.map((length, index) =>
-					index == paraIndex
-						? <div className="word-count active-count">{length}</div>
-						: <div className="word-count" onClick={() => setParaIndex(index)}>{length}</div>
+					index === paraIndex
+						? <div className="word-count active-count" key={index}>{length}</div>
+						: <div className="word-count" key={index} onClick={() => setParaIndex(index)}>{length}</div>
 				)}
 				<div className="word-count"><i className="material-icons-round"> handyman </i></div>
 			</div>
