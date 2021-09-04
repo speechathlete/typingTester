a2e550296636cba06e02c039994aa1ba839e3862
Vatsal Gupta <vatsalgupta280@gmail.com>
2021-09-04 11:21:01 +0530

    added dummy config file

---
+++ src/firebaseConfig-rename-me.js
@@ -0,0 +1,12 @@
+// Add firebase config
+
+const firebaseConfig = {
+	apiKey: "",
+	authDomain: "",
+	projectId: "",
+	storageBucket: "",
+	messagingSenderId: "",
+	appId: ""
+};
+
+export default firebaseConfig;
