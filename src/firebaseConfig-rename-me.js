1fe83db49336648669b2b925aa27a7a8ba8b09c7
Anany-Gupta <ananygupta@gmail.com>
2021-09-06 12:33:08 +0530

    Added AuthProvider, changes in firebaseConfig file

--- src/firebaseConfig-rename-me.js
+++ src/firebaseConfig-rename-me.js
@@ -1,12 +1,24 @@
-// Add firebase config
-
+import firebase from 'firebase/compat/app';
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+import "firebase/compat/storage";
 const firebaseConfig = {
 	apiKey: "",
-	authDomain: "",
+	authDomain:"" ,
 	projectId: "",
-	storageBucket: "",
-	messagingSenderId: "",
-	appId: ""
+	storageBucket:"",
+	messagingSenderId:"" ,
+	appId: 
+};
+firebase.initializeApp(firebaseConfig);
+
+let provider = new firebase.auth.GoogleAuthProvider();
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
+export const signInWithGoogle = () => {
+	auth.signInWithPopup(provider);
 };
 
-export default firebaseConfig;
+export default firebase;
